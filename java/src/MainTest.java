c699e4fe519b3345cfdc626fc11fd86a71672337
tangxiong <tangx@chinawiserv.com>
2020-09-22 22:25:56 +0800

    数组学习

--- java/src/MainTest.java
+++ java/src/MainTest.java
@@ -1,5 +1,20 @@
 public class MainTest {
-    public static void main(String[] args) {
+    /**
+     * 数组特性
+     * 1.线性表
+     * 2.连续的内存空间               随机访问
+     * 3.相同数据类型
+     *
+     * 数组可以借助cpu缓存机制，预读数组中的数据
+     *
+     * 数组是适合查找操作，但是查找的时间复杂度并不为 O(1)。即便是排好序的数组，你用二分查找，时间复杂度也是 O(logn)。
+     * 所以，正确的表述应该是，数组支持随机访问，根据下标随机访问的时间复杂度为 O(1)
+     *
+     *
+     * @param args
+     */
+ public static void main(String[] args) {
         System.out.println("hello");
-    }
-}
+     }
+ }
+
